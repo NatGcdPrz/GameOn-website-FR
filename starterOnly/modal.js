917a82baccb398bafcefd5843254a794e0eb702c
Nathalie Perez <natgcdprz@icloud.com>
2023-01-04 11:40:11 +0100

    Fix bug #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Pas de modif mais var n'est plus utilisé - x c'est quoi ?
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,6 +12,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,15 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal btn
+closeBtn.addEventListener("click", () => {
+  modalbg.style.display = "none";
+})
 
+/* Exemple à ne pas faire (vieille méthode) 
+ajoute un listener à toute la page alors qu'on a besoin que du bouton (quand je clique partout sur la page la console renvoie l'info)
+window.onclick = function (event) {
+  if (event.target == closeBtn) {
+    modalbg.style.display = "none";
+  }
+} */
