35c66555f99c7df88ddf5f19bc762168b4c2be86
Nathalie Perez <natgcdprz@icloud.com>
2023-02-06 11:52:02 +0100

    Issue #2 add input form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,20 +28,69 @@ function FormValidate() {
 
 }
 
-// ----------------- ERRORS FUNCTIONS ----------------- // 
-// Function for error messages
-const displayErrorMsg = (element, message) => {
-    const formData = element
-    formData.setAttribute("data-error", message)
-    formData.setAttribute("data-error-visible", true)
+// Function for validation
+function validate(formData) {
+
+    // All Regex
+    const stringRegex = /^[a-zA-Z-]+$/;
+    const regexEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    const regexBirthDate = new RegExp(/^\d{4}[-](0[1-9]|1[0-2])[-](0?[12][0-9]|3[01])$/);
+
+    let isFormValid = true;
+
+    // First name
+    if (!formData[0].children[2].value.match(stringRegex) || formData[0].children[2].value.length < 2) {
+        isFormValid = false;
+    }
+
+    ///Last name
+    if (!formData[1].children[2].value.match(stringRegex) || formData[1].children[2].value.length < 2) {
+        isFormValid = false;
+    }
+
+    // Check email is valid
+    if (!formData[2].children[2].value.match(regexEmail)) {
+        isFormValid = false;
+    }
+
+    // Check the birthdate
+    if (!formData[3].children[2].value.match(regexBirthDate)) {
+        isFormValid = false;
+    }
+
+    // Check number is valid
+    // Need a number not a string
+    if (formData[4].children[2].value === "") {
+        isFormValid = false;
+    }
+
+    // Check radio btn
+    // Need a city for validate form
+    const radio = document.getElementsByName('location');
+    let citySelectionned = false;
+
+    for (input of radio) {
+        if (input.checked) citySelectionned = true;
+    }
+
+    if (!citySelectionned) {
+        isFormValid = false;
+    }
+
+    // Check cgu
+    // Need to checked the cgu
+    if (!(document.querySelector("#checkbox1").checked)) {
+        isFormValid = false;
+    }
+
+    // Check previous error
+    if (isFormValid) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-// Function for remove one error msg
-const removeErrorMsg = (element) => {
-    const formData = element
-    formData.removeAttribute('data-error')
-    formData.removeAttribute('data-error-visible')
-}
 
 // ----------------- MODAL FUNCTIONS & EVENTS ----------------- // 
 // Functions for modal
